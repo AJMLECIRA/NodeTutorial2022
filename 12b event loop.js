61638560689f138781d3193bec1a93fa7b1a340a
AJMLECIRA <andrew@mirrorworld.co.uk>
2021-09-29 09:33:28 +0100

    wednesday 29th

---
+++ 12b event loop.js
@@ -0,0 +1,8 @@
+// start opering system process
+console.log('this is done first')
+setTimeout(() => {
+    console.log('this is done second')
+},0)
+
+console.log('this is done third')
+//complete and exicuted system precess
---
+++ 12c terminal loop.js
@@ -0,0 +1,7 @@
+setInterval(() => {
+    console.log('Hello world')
+},2000)
+console.log('I will run first')
+//process stays alive umless
+// kill process CONTROL + C
+// unexpected error
